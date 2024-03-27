6ae2e1b4a008d8b6d20dc6b4ea8f6ae80d71ecad
Manasa0027 <161139812+Manasa0027@users.noreply.github.com>
2024-03-27 19:55:14 +0530

    Add files via upload

---
+++ OrderDelivery.java
@@ -0,0 +1,63 @@
+package week8;
+
+import java.util.Scanner;
+public interface OrderDelivery
+{
+	public void deliverOrder();
+}
+class Swiggy implements OrderDelivery
+{	
+	String cust_name;
+	byte orderID;
+	Swiggy () 
+	{
+		Scanner inScanner = new Scanner(System.in);
+		System.out.println("Enter the CustomerName...");
+		cust_name=inScanner.next();
+		System.out.println("Enter the orderID...");
+		orderID=inScanner.nextByte();
+    }
+	public void deliverOrder() 
+	{
+		System.out.println("Delivering food order of Mrs."+cust_name +" with order number"+orderID+".");
+    }
+}
+class Swiggy_Instamart implements OrderDelivery
+{
+	String cust_name;
+	byte orderID;
+	int product_qty;
+	Swiggy_Instamart() 
+	{
+		Scanner inScanner = new Scanner(System.in);
+		System.out.println("Enter the Customer Name...");
+		cust_name=inScanner.next();
+		System.out.println("Enter the order ID...");
+		orderID=inScanner.nextByte();
+		System.out.println("Enter the product quantity...");
+		product_qty=inScanner.nextInt();
+	}
+    public void deliverOrder() 
+    {
+    	System.out.println("Delivering Grocery order of Mrs. "+cust_name + " with order number\t"+orderID+". \nThe total no of products delivered are "+product_qty);
+	}
+}
+class Delivery
+{
+	public void deliverToCustomer (OrderDelivery od) 
+	{ 
+	od.deliverOrder();
+	}
+}
+class DemoOCP 
+{
+	public static void main(String args[])
+	{
+		Delivery order = new Delivery();
+		Swiggy sw = new Swiggy();
+		order.deliverToCustomer(sw);
+		Swiggy_Instamart insta = new Swiggy_Instamart();
+		order.deliverToCustomer (insta);
+	}
+
+}
