82940d73903e6f952529a5df60bd32cc3d08b6ce
Manasa0027 <161139812+Manasa0027@users.noreply.github.com>
2024-04-04 16:05:02 +0530

    Add files via upload

--- student.java
+++ student.java
@@ -1,34 +1,57 @@
-package week2;
+package week9;
 
 public class student 
 {
-	String studname;
-	String dept;
-	int rollno;
-	static String college="GPT";
-	int percentage;
-	void setstudent(String name , int rno , String cdept , int per)
+	String name;
+	float[] CIE= {0,0,0};
+	float avg;
+	byte rollNo;
+	void setstudent(String n,float ia[],byte rno)
 	{
-	studname=name;
-	rollno = rno;
-	dept=cdept;
-	percentage = per;
+		float sum=0.0f;
+		name=n;
+		avg=0.0f;
+		rollNo=rno;
+		for(byte i=0;i<3;i++)
+		{
+			CIE[i]=ia[i];
+			sum=sum+CIE[i];
+		}
+		avg=sum/0.0f;
 	}
-	void getstudent()
+	void setstudent(String n,float ia1,float ia2,byte no)
 	{
-	System.out.println(studname+"\t\t"+rollno+"\t"+college+"\t\t"+dept+"\t\t"+percentage);
+		name=n;
+		rollNo=no;
+		CIE[0]=ia1;
+		avg=(CIE[0]+CIE[1]/3.0f);
 	}
+	void setstudent(String n ,float ia,byte rno)
+	{
+		name=n;
+		rollNo=rno;
+		CIE[0]=ia;
+		avg=(CIE[0])/3.0f;
 	}
-	class stud
+	void showstudent()
 	{
-	public static void main(String args[])
-	{
-	System.out.println("name\t\trollno\tcollege\t\tdepartment\tpercentage");
-	student stud1=new student();
-	student stud2=new student();
-	stud1.setstudent("manasa",165,"CS",88);
-	stud2.setstudent("inchara",100,"EC",90);
-	stud1.getstudent();
-	stud2.getstudent();
+		System.out.println(rollNo+"\t\t"+name+"\t\t"+avg+"\t\t");
 	}
 }
+class demoMethodoverloading
+{
+	public static void main(String args[])
+	{
+		student mi=new student();
+		System.out.println("The internal details of a student.");
+		System.out.println("RollNo\t\tName\t\tAverage");
+		mi.setstudent("manasa",19.4f ,(byte)12);
+		mi.showstudent();
+		mi.setstudent("inchara", 19.4f, (byte)10);
+		mi.showstudent();
+		float[]CIE= {17.0f,12.6f,19.2f};
+		mi.setstudent("harshi",19.5f,(byte)8);
+		mi.showstudent();
+	}
+}
+
